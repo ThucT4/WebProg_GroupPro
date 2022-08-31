774673d9247e45ef6dc8163d2a674bb63d07d67e
ThucT4 <94610073+ThucT4@users.noreply.github.com>
2022-08-31 20:36:45 +0700

    Add files via upload

---
+++ src/components/registerPage/register.php
@@ -0,0 +1,48 @@
+<?php
+    session_start();
+
+include_once('../../../server/write2file.php');
+include_once('../../../server/readFromfile.php');
+include_once('../../../server/classes/account.php');
+
+global $accounts;
+    $accounts = readFromFile('accounts.txt');
+    
+if (isset($_POST['create-account'])) {
+    //print_r($_POST);
+    if (!validate($_POST)) {
+        echo "<script type='text/javascript'> alert('Username has been used. PLease use another username!');</script>";
+    }
+    else {
+        $temp = new Account($_POST['username'], password_hash($_POST['password'], PASSWORD_DEFAULT)
+        , $_POST['avt'], $_POST['account-type'], $_POST['address']
+        , $_POST['buss-name'], $_POST['buss-address'], $_POST['distribution-hub']);
+
+        writeToFile($temp, "accounts.txt");
+
+        header('location: ../loginPage/login.php');
+    }
+}
+
+    loadPage();
+
+    function loadPage() {
+        //Header
+        require_once("../../../src/components/header/header.html");
+
+        //Main
+        require_once("../../../src/components/registerPage/customerRegister.html");
+
+        //Footer
+        //require_once("../../../src/components/footer/footer.html");
+    }
+
+    function validate($input) {
+        foreach ($GLOBALS['accounts'] as $account) {
+            if ($input['username'] === $account->username) {
+                return false;
+            }
+        }
+        return true;
+    }
+?>
