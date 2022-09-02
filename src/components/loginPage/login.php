2037ca10d100648c3eafa2e613194dbdd208be15
ThucT4 <94610073+ThucT4@users.noreply.github.com>
2022-09-02 10:13:31 +0700

    Update login.php

--- src/components/loginPage/login.php
+++ src/components/loginPage/login.php
@@ -7,23 +7,36 @@ include_once('../../../server/classes/account.php');
 global $accounts, $type, $username;
     $accounts = readFromFile('accounts.txt');
 
+//unset($_SESSION['user']);
+//unset($_SESSION['accountType']);
+if (isset($_SESSION['user']))  {
+    echo $type;
+    header('location: ../customerPage/customerPage.php');
+    if ($_SESSION['accountType'] === 'customer') {
+        header('location: ../customerPage/customerPage.php');
+    }
+    else if ($_SESSION['accountType'] === "vendor") {
+        //header('location: ../vendorPage/vendor.php');
+    }
+    else {
+        //header('location: ../shipperPage/shipper.php');
+    }
+}
+
 if (isset($_POST['login'])) {
     //print_r($_POST);
     if (!authenticate($_POST)) {
         echo "<script type='text/javascript'> alert('Username or password is incorrect. PLease check again!');</script>";
     }
     else {
-        setcookie('user', $GLOBALS['username'], time() + 3600);
+        $_SESSION['start'] = time();
+        $_SESSION['user'] = $GLOBALS['username'];
+        $_SESSION['accounttype'] = $GLOBALS['type'];
+        $_SESSION['expire'] = $_SESSION['start'] + (120 * 60); //Expire after 30m
 
-        if ($GLOBALS['type'] === 'customer') {
-            header('location: ../customerPage/customerPage.php');
-        }
-        else if ($GLOBALS['type'] == "vendor") {
-            //header('location: ../vendorPage/vendor.php');
-        }
-        else {
-            //header('location: ../shipperPage/shipper.php');
-        }
+        header('location: ../customerPage/customerPage.php');
+
+        unset($_POST['login']);
     }
 }
 
